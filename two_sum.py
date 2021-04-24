54b22b4e8a998bf7f59e807556b294e188c9a18c
codify21 <mac95shivam@gmail.com>
2021-04-25 04:15:49 +0530

    twosum

---
+++ two_sum.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 25 03:53:12 2021
+
+@author: macbookpro
+"""
+
+class Solution:
+    def twoSum(self, nums , target: int) :
+        
+        mydict = {}
+        
+        for i,num in enumerate(nums):
+            print(str(i) + ' ' +str(num))
+            complement = target - num
+            if complement in mydict:
+                return [mydict[complement], i]
+            else:
+                mydict[num] = i
+                
+sum = Solution()
+z=sum.twoSum([2,5,7,9],11)
+print(z)
