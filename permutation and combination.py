d973047779ca2c1e891e9884cd19b011b6056cfd
codify21 <mac95shivam@gmail.com>
2022-07-23 19:33:17 +0530

    combinations

--- permutation and combination.py
+++ permutation and combination.py
@@ -36,6 +36,34 @@ class Solution:
 
         
 sol1 = Solution()
-output = sol1.permute([1,1,2,3,])
+output = sol1.permute([1,2,3,4])
+
+print(output)
+
 for i in output:
     print(i)
+    
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res=[]
+        
+        def backtrack(start,comb):
+            if len(comb)==k:
+                res.append(comb.copy())
+                return
+            
+            for i in range(start,n+1):
+                comb.append(i)
+                backtrack(i+1,comb)
+                comb.pop()
+        
+        backtrack(1,[])
+        return res
+    
+# import itertools
+# def combine(self, n: int, k: int) -> List[List[int]]: # one line solution
+#     return itertools.combinations(range(1, n + 1), k)
+
+sol1 = Solution()
+output = sol1.combine(4,2)
+print(output)
