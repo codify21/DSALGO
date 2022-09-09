e24c8cd029a45b0be7454e5f88192930ce5001f4
codify21 <mac95shivam@gmail.com>
2022-09-09 18:26:24 +0530

    binary tree pruning

--- interrview.py
+++ interrview.py
@@ -137,15 +137,179 @@
 
 
 
+# def solution(root, l, c):     #  l -> level number  and  c -> column number
+#             if root is None:
+#                 return
+#             mapp[(c,l)].append(root.val)
+#             solution(root.left, l + 1, c - 1)
+#             solution(root.right, l + 1, c + 1)
+            
+#         mapp = defaultdict(list)
+#         solution(root, 0, 0)
+#         res = []
+#         old = float("-inf")
         
+#         for k, v in sorted(mapp.items()):
+            
+#             if k[0] != old:   # to check whenever a new index/vertical column starts
+#                 res.append([])
+                
+#             res[-1].extend(sorted(v))
+#             old = k[0]
+#         return res
 
-for i in range(5):
-    
-    for j in range(4,0,-1):
+
+
+
+
+# arr = [5,2,3,5,4,3]
+# # [5,2,3][2,3,4,5],[5,4,3],[4,3][3]
+
+
+# res=[]
+# res1=[]
+
+# for i in range(len(arr)):
+#     for j in range(i,len(arr)):
+#         if arr[j] not in res:
+#             res.append(arr[j])
+#         else:
+#             break
+#     res1.append(res)
+#     res=[]
+   
+# print(res1)
+# length=0
+# flag=0
+# for i in res1:
+#     temp=len(i)
+#     if temp>length:
+#         length=temp
+#         flag=i
         
-        print(" ",end="")
-    d= "* "*i
-    print(d)
+# print(flag)
+
+
+
+
+
+
+
+
+
+
+# def check_divide(func):
+#     def inner(a,b):
+#         if b==0:
+#             print ("Division is not possible")
+#         return func
+        
+        
+#     inner()
+    
+
+# @check_divide
+# def divide(a,b):
+#     print('After division',(a/b))
+    
+    
+
+# divide(5,7)
+
+
+
+
+
+
+# comp = [i for i in range(1,10)]
+
+# print(comp)
+
+
+
+
+# class A:
+    
+#     def sum(self,a,b):
+#          print(a+b)
+         
+# class B(A):
+    
+#     def sum(self,a,b):
+#         return(a+b+10)
+        
+# test = B()
+# print(test.sum(5,7))    
+
+
+
+
+
+# select  A.id ,A.value ,B.value from  A, B  right join on A.id = B.id 
+
+
+
+# class Publisher(models.Model):
+    
+#     name = models.TextAreaField()
+#     security_no. = models.IntegerFied()
+#     book = models.CharField()
+#     Best_seller =models.BooleanField()
+#     price=models.DoubleField()
+    
+    
+    
+
+
+
+
+
+
+
+class A:
+    
+    def summ(a,b):
+        
+        
+        return a+b
+    
+class B(A):
+    
+    def summ(a,b):
+        
+        return a+b+10
+
+
+
+
+lis= [3,4,3,4,5,1,5,2,3]
+
+
+out = lis.sort()
+
+item=out[-1]
+flag=0
+for i in range(len(lis)-1,-1,-1):
+    if out[i]==item:
+        flag+=1
+        continue
+    else:
+        break
+    
+print("Occurence",flag)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
